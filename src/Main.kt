b303e0782f5b8393b23f28a05cc2e3df34455386
Cavin <cavinlarry001@gmail.com>
2025-01-09 01:05:26 +0300

    Solved stage 1

--- src/Main.kt
+++ src/Main.kt
@@ -1,4 +1,14 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 fun main() {
+    val message =
+        """
+        I love animals!
+        Let's check on the animals...
+        The deer looks fine.
+        The bat looks happy.
+        The lion looks healthy.
+    """.trimIndent()
+
+    println(message)
 }
